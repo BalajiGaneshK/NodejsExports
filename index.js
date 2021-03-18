8cd4e761a19b15738626f016c98cac5f40f76914
BalajiGaneshK <kbalajiganeshdav@gmail.com>
2021-03-18 16:34:29 +0530

    Calculator

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let operations = require("./calculations");
+let num1 = 1000;
+let num2 = 5;
+
+console.log("Addition:", operations.add(num1, num2));
+console.log("Subtraction:", operations.sub(num1, num2));
+console.log("Multiplication:", operations.mul(num1, num2));
+console.log("Division:",operations.div(num1, num2));
