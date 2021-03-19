56606d48bce56b05f6906a452a59958998f6d9e0
BalajiGaneshK <69104773+BalajiGaneshK@users.noreply.github.com>
2021-03-19 08:45:35 +0530

    Module exports

---
+++ calculations.js
@@ -0,0 +1,22 @@
+function add(op1, op2)
+{
+    return op1 + op2;
+}
+
+function sub(op1, op2)
+{
+    return op1 - op2;
+}
+
+function mul(op1, op2)
+{
+    return op1*op2;
+}
+
+function div(op1, op2)
+{
+    return op1/op2;
+}
+
+module.exports = { add, sub, mul, div };
+
